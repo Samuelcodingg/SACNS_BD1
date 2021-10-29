bd25afd3186059f33fb32648622372d1b0b9720b
Samuelcodingg <samuelroman39@gmail.com>
2021-10-28 20:27:17 -0500

    se inició renderizacion de volver en loginpage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,39 @@
 const btnAlumno = document.getElementById('btnAlumno');
+const btnDocente = document.getElementById('btnDocente');
 const containerBtnRoles = document.getElementById('containerBtnRoles')
 const titleLoginPage = document.getElementById('titleLoginPage');
 const textLoginPage = document.getElementById('textLoginPage');
 const containerLoginPageRight = document.getElementById('containerLoginPageRight');
 
 document.addEventListener('DOMContentLoaded', () => {
-    btnAlumno.addEventListener('click', showInitialOptions);
 
+    btnAlumno.addEventListener('click', showLoginAlumno);
+
+    btnDocente.addEventListener('click', showLoginDocente);
 })
 
-const showInitialOptions = () => {
+const showLoginAlumno = () => {
     containerBtnRoles.classList.add('d-none');
     titleLoginPage.textContent = 'Iniciar Sesión';
     textLoginPage.textContent = 'Bienvenido, coloque su ID y Contraseña';
 
-    renderLoginForm();
-
+    renderLoginForm('idFormAlumno');
+    renderVolver();
 }
 
-const renderLoginForm = () => {
+const showLoginDocente = () => {
+    containerBtnRoles.classList.add('d-none');
+    titleLoginPage.textContent = 'Iniciar Sesión';
+    textLoginPage.textContent = 'Bienvenido, coloque su ID y Contraseña';
+
+    renderLoginForm('idFormDocente');
+}
+
+const renderLoginForm = (idPerfilForm) => {
     const formLogin = document.createElement('form');
+    formLogin.setAttribute('method', 'POST');
+    formLogin.setAttribute('id', idPerfilForm);
+
     const formContent = `
         <div class="mb-4">
             <p class="label-color">ID</p>
@@ -35,4 +49,20 @@ const renderLoginForm = () => {
     `
     formLogin.innerHTML = formContent;
     containerLoginPageRight.appendChild(formLogin);
+}
+
+const renderVolver = () => {
+    const divVolver = document.createElement('div');
+    divVolver.classList.add('d-flex','align-items-center','mb-5','pointer');
+
+    const contentDiv = `
+        <svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M9.8627 2.225L8.37936 0.75L0.137695 9L8.3877 17.25L9.8627 15.775L3.0877 9L9.8627 2.225Z" fill="#8692A6"/>
+        </svg>
+
+        <p class="mb-0 ms-2 text-secondary">Volver</p>
+    `
+
+    divVolver.innerHTML = contentDiv;
+    containerLoginPageRight.parentElement.insertBefore(divVolver, containerLoginPageRight);
 }
